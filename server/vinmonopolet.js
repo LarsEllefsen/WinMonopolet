d5e510da679caebb682f5343ab382259bc7899e1
LarsEllefsen <larsmoeellefsen@gmail.com>
2018-04-20 19:07:17 +0200

    New api endpoint

--- server/vinmonopolet.js
+++ server/vinmonopolet.js
@@ -1,10 +1,31 @@
 const vinmonopolet = require('vinmonopolet')
 
-vinmonopolet.getFacets().then(facets => {
-  const storeFacet = facets.find(facet => facet.name === 'Butikker')
-  const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
 
-  return vinmonopolet.getProducts({limit: 3, facet: storeFacetValue})
-}).then(response => {
-  console.log(response.products) // 3 products from Norway
+//vinmonopolet.getFacets().then(facets => {
+  //const storeFacet = facets.find(facet => facet.name === 'Butikker')
+  //const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
+
+  //const productFacet = facets.find(facet => facet.name === 'productCategory')
+  //const beer = vinmonopolet.Facet.Category.BEER
+
+  //return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
+//}).then(response => {
+  //console.log(response.products) // 3 products from Norway
+//})
+
+async function getAllCiders() {
+
+  let {pagination, products} = await vinmonopolet.getProducts({facet: [storeFacetValue, vinmonopolet.Facet.Category.BEER]})
+
+  while (pagination.hasNext) {
+    const response = await pagination.next()
+    products = products.concat(response.products)
+    pagination = response.pagination
+  }
+
+  return products
+}
+
+getAllCiders().then(allProducts => {
+  console.log(allProducts)
 })
