630bef79241e53afe2326c9bc7f2d323136477af
LarsEllefsen <larsmoeellefsen@gmail.com>
2018-09-20 14:35:12 +0200

    merging

--- server/vinmonopolet.js
+++ server/vinmonopolet.js
@@ -1,31 +1,54 @@
 const vinmonopolet = require('vinmonopolet')
+const sqlite3 = require('sqlite3')
 
-
-//vinmonopolet.getFacets().then(facets => {
-  //const storeFacet = facets.find(facet => facet.name === 'Butikker')
-  //const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
-
-  //const productFacet = facets.find(facet => facet.name === 'productCategory')
-  //const beer = vinmonopolet.Facet.Category.BEER
-
-  //return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
-//}).then(response => {
-  //console.log(response.products) // 3 products from Norway
-//})
-
-async function getAllCiders() {
-
-  let {pagination, products} = await vinmonopolet.getProducts({facet: [storeFacetValue, vinmonopolet.Facet.Category.BEER]})
-
-  while (pagination.hasNext) {
-    const response = await pagination.next()
-    products = products.concat(response.products)
-    pagination = response.pagination
+let db = new sqlite3.Database('inv.db', (err) => {
+  if (err) {
+    console.error(err.message);
   }
+  console.log('Connected to the inventory database.');
+});
 
-  return products
-}
+db.run('CREATE TABLE IF NOT EXISTS beers(id INTEGER PRIMARY KEY ON CONFLICT IGNORE, name TEXT NOT NULL, type TEXT NOT NULL, price REAL NOT NULL, score REAL NOT NULL, stockLevel INTEGER NOT NULL)');
 
-getAllCiders().then(allProducts => {
-  console.log(allProducts)
+
+vinmonopolet.getFacets().then(facets => {
+  const storeFacet = facets.find(facet => facet.name === 'Butikker')
+  const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
+
+  const productFacet = facets.find(facet => facet.name === 'productCategory')
+  const beer = vinmonopolet.Facet.Category.BEER
+
+  return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
+}).then(response => {
+  for(i=0; i<response.products.length; i++) {
+    if(response.products[i].chosenStoreStock.stockLevelStatus == 'inStock') {
+      var code = response.products[i].code
+      var name = response.products[i].name
+      var type = response.products[i].mainSubCategory.name
+      var stockLevel = response.products[i].chosenStoreStock.stockLevel
+      var price = response.products[i].price
+      // console.log(type)
+      db.run('INSERT INTO beers VALUES (?,?,?,?,?,?)', [code,name,type,price,69.0,stockLevel])
+    }
+  }
+  console.log(response.products[0])
+  // console.log(response.products) // 3 products from Norway
 })
+
+//====================
+// async function getAllCiders() {
+//
+//   let {pagination, products} = await vinmonopolet.getProducts({facet: [storeFacetValue, vinmonopolet.Facet.Category.BEER]})
+//
+//   while (pagination.hasNext) {
+//     const response = await pagination.next()
+//     products = products.concat(response.products)
+//     pagination = response.pagination
+//   }
+//
+//   return products
+// }
+//
+// getAllCiders().then(allProducts => {
+//   console.log(allProducts)
+// })
