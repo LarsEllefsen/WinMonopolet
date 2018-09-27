003301b5bc89476c97e6b448aa55770ea68958ba
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2018-09-27 15:30:33 +0200

    removed some git stuff that I overlooked

--- server/vinmonopolet.js
+++ server/vinmonopolet.js
@@ -1,7 +1,6 @@
 const vinmonopolet = require('vinmonopolet')
 const sqlite3 = require('sqlite3')
 
-<<<<<<< HEAD
 let db = new sqlite3.Database('inv.db', (err) => {
   if (err) {
     console.error(err.message);
@@ -33,58 +32,4 @@ vinmonopolet.getFacets().then(facets => {
     }
   }
   console.log(response.products[0])
-  // console.log(response.products) // 3 products from Norway
-=======
-
-//vinmonopolet.getFacets().then(facets => {
-  //const storeFacet = facets.find(facet => facet.name === 'Butikker')
-  //const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
-
-  //const productFacet = facets.find(facet => facet.name === 'productCategory')
-  //const beer = vinmonopolet.Facet.Category.BEER
-
-  //return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
-//}).then(response => {
-  //console.log(response.products) // 3 products from Norway
-//})
-
-
-async function getAllBeers() {
-
-  const facets = await vinmonopolet.getFacets();
-  const AllStores = facets.find(facet => facet.title == 'stores');
-  const store = AllStores.values.find(store => store.name == 'Trondheim, Bankkvartalet');
-
-  let {pagination, products} = await vinmonopolet.getProducts({facet: [store, vinmonopolet.Facet.Category.BEER]})
-
-  while (pagination.hasNext) {
-    const response = await pagination.next()
-    products = products.concat(response.products)
-    pagination = response.pagination
-  }
-
-  return products
-}
-
-getAllBeers().then(allProducts => {
-  console.log(allProducts)
->>>>>>> dca85c94a6d976c03662d7dac772ff6686339eda
 })
-
-//====================
-// async function getAllCiders() {
-//
-//   let {pagination, products} = await vinmonopolet.getProducts({facet: [storeFacetValue, vinmonopolet.Facet.Category.BEER]})
-//
-//   while (pagination.hasNext) {
-//     const response = await pagination.next()
-//     products = products.concat(response.products)
-//     pagination = response.pagination
-//   }
-//
-//   return products
-// }
-//
-// getAllCiders().then(allProducts => {
-//   console.log(allProducts)
-// })
