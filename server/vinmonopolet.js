3e2e604407d2969bef42fd25aad175bf8e851f45
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2018-10-03 16:23:37 +0200

    added variable tablenames depending on the store you're querying

--- server/vinmonopolet.js
+++ server/vinmonopolet.js
@@ -8,28 +8,44 @@ let db = new sqlite3.Database('inv.db', (err) => {
   console.log('Connected to the inventory database.');
 });
 
-db.run('CREATE TABLE IF NOT EXISTS beers(id INTEGER PRIMARY KEY ON CONFLICT IGNORE, name TEXT NOT NULL, type TEXT NOT NULL, price REAL NOT NULL, score REAL NOT NULL, stockLevel INTEGER NOT NULL)');
-
-
-vinmonopolet.getFacets().then(facets => {
-  const storeFacet = facets.find(facet => facet.name === 'Butikker')
-  const storeFacetValue = storeFacet.values.find(val => val.name === 'Trondheim, Bankkvartalet')
-
-  const productFacet = facets.find(facet => facet.name === 'productCategory')
-  const beer = vinmonopolet.Facet.Category.BEER
-
-  return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
-}).then(response => {
-  for(i=0; i<response.products.length; i++) {
-    if(response.products[i].chosenStoreStock.stockLevelStatus == 'inStock') {
-      var code = response.products[i].code
-      var name = response.products[i].name
-      var type = response.products[i].mainSubCategory.name
-      var stockLevel = response.products[i].chosenStoreStock.stockLevel
-      var price = response.products[i].price
-      // console.log(type)
-      db.run('INSERT OR IGNORE INTO beers VALUES (?,?,?,?,?,?)', [code,name,type,price,69.0,stockLevel])
-    }
+//Some store names contain commas, this function checks if that is the case, if so, formats it for sqlite3.
+function formatName(name) {
+  if(name.includes(",")){
+    return name.replace(/, /g, '_');
+  } else {
+    return name
   }
-  console.log(response.products[0])
-})
+}
+
+//Gets
+function getBeersByStore(store){
+  const tableName = formatName(store)
+  console.log(tableName)
+  db.run('CREATE TABLE IF NOT EXISTS '+tableName+' (id INTEGER PRIMARY KEY ON CONFLICT IGNORE, name TEXT NOT NULL, type TEXT NOT NULL, price REAL NOT NULL, score REAL NOT NULL, stockLevel INTEGER NOT NULL)');
+
+  vinmonopolet.getFacets().then(facets => {
+    const storeFacet = facets.find(facet => facet.name === 'Butikker')
+    const storeFacetValue = storeFacet.values.find(val => val.name === store)
+
+    const productFacet = facets.find(facet => facet.name === 'productCategory')
+    const beer = vinmonopolet.Facet.Category.BEER
+
+    return vinmonopolet.getProducts({facet: [storeFacetValue, beer]})
+  }).then(response => {
+    for(i=0; i<response.products.length; i++) {
+      if(response.products[i].chosenStoreStock.stockLevelStatus == 'inStock') {
+        var code = response.products[i].code
+        var name = response.products[i].name
+        var type = response.products[i].mainSubCategory.name
+        var stockLevel = response.products[i].chosenStoreStock.stockLevel
+        var price = response.products[i].price
+        // console.log(type)
+        db.run('INSERT OR IGNORE INTO '+tableName+' VALUES (?,?,?,?,?,?)', [code,name,type,price,69.0,stockLevel])
+      }
+    }
+    console.log(response.products[0])
+    console.log(response.pagination)
+  })
+}
+
+getBeersByStore('Trondheim, Bankkvartalet')
