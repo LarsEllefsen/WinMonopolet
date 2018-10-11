94a2b9a635dd9b3faf939839e1c3535f39a5844d
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2018-10-11 17:54:08 +0200

    Added a Stores table, and logic for freshness checking.

--- server/vinmonopolet.js
+++ server/vinmonopolet.js
@@ -6,6 +6,11 @@ let db = new sqlite3.Database('inv.db', (err) => {
     console.error(err.message);
   }
   console.log('Connected to the inventory database.');
+  db.run('CREATE TABLE IF NOT EXISTS stores (store_id INTEGER PRIMARY KEY, name TEXT NOT NULL, last_updated TEXT NOT NULL)', function(err) {
+    if (err){
+      console.log(err.message)
+    }
+  });
 });
 
 //Some store names contain commas, this function checks if that is the case, if so, formats it for sqlite3.
@@ -17,8 +22,22 @@ function formatName(name) {
   }
 }
 
-//Gets
-function getBeersByStore(store){
+
+function createOrUpdate(store, exists){
+  var date = new Date();
+  if(exists){
+    db.run('UPDATE stores SET last_updated = ? WHERE name = ?', [date,store])
+  } else {
+    db.run('INSERT INTO stores VALUES (?,?,?)', [null,store,date], function(err) {
+      if (err){
+        console.log(err.message)
+      }
+    })
+  }
+}
+
+//Gets all beers from the selected store and inserts into table.
+function getBeersByStore(store, exists){
   const tableName = formatName(store)
   console.log(tableName)
   db.run('CREATE TABLE IF NOT EXISTS '+tableName+' (id INTEGER PRIMARY KEY ON CONFLICT IGNORE, name TEXT NOT NULL, type TEXT NOT NULL, price REAL NOT NULL, score REAL NOT NULL, stockLevel INTEGER NOT NULL)');
@@ -46,6 +65,25 @@ function getBeersByStore(store){
     console.log(response.products[0])
     console.log(response.pagination)
   })
+  createOrUpdate(store,exists)
 }
 
-getBeersByStore('Trondheim, Bankkvartalet')
+//Checks the store against the store table to see when it was last updated.
+//If it exists and is fresher than a set threshold (tbd), select from DB, else getBeersByStore
+function check_store(store){
+  db.get('SELECT * FROM stores WHERE name=?', [store], (err, row) =>{
+    if(err){
+      console.log(err.message)
+    } else {
+      //If undefined, the store has yet to be created.
+      if(row == undefined) {
+        getBeersByStore(store, false)
+      } else {
+        console.log(row)
+      }
+    }
+  });
+}
+
+check_store('Trondheim, Bankkvartalet')
+//getBeersByStore('Trondheim, Bankkvartalet')
