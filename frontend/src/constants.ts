ebebe34f1c2c50d5237344bb50ccc102b347fad4
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2024-01-21 19:00:58 +0100

    Changed callback url to production

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,1 @@
-export const UNTAPPD_AUTH_CALLBACK_URL = `http://127.0.0.1:5173/auth/callback/untappd`;
+export const UNTAPPD_AUTH_CALLBACK_URL = `https://www.winmonopolet.no/auth/callback/untappd`;
