ebadeb5205ff514dc5891375a78941877ee3c204
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2024-05-13 20:10:25 +0200

    Remove debug logging from production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,21 @@
-// import 'reflect-metadata';
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { LogLevel, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api");
-  app.useGlobalPipes(new ValidationPipe());
+	const logLevels = ['log', 'warn', 'error'] satisfies LogLevel[];
+	const debugLogging =
+		process.env.NODE_ENV === 'development'
+			? (['debug'] satisfies LogLevel[])
+			: [];
 
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule, {
+		logger: [...logLevels, ...debugLogging],
+	});
+
+	app.setGlobalPrefix('api');
+	app.useGlobalPipes(new ValidationPipe());
+
+	await app.listen(3000);
 }
 bootstrap();
