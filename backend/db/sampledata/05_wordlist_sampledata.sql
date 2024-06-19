ba4fceaceec8729f6c22daadb40af103f96c031a
Lars Moe Ellefsen <larsmoeellefsen@gmail.com>
2024-06-19 21:18:02 +0200

    Added sampledata

---
+++ backend/db/sampledata/05_wordlist_sampledata.sql
@@ -0,0 +1,26 @@
+INSERT INTO public.wordlist (value) VALUES
+	 ('DIPA'),
+	 ('NEIPA'),
+	 ('X'),
+	 ('DDH'),
+	 ('IPA'),
+	 ('Barley Wine'),
+	 ('west coast'),
+	 ('Double'),
+	 ('TIPA'),
+	 ('India Pale Ale');
+INSERT INTO public.wordlist (value) VALUES
+	 ('American'),
+	 ('pastry'),
+	 ('sour'),
+	 ('new zealand'),
+	 ('juicy'),
+	 ('pale ale'),
+	 ('Imperial Stout'),
+	 ('session'),
+	 ('Imperial Gose'),
+	 ('Imperial');
+INSERT INTO public.wordlist (value) VALUES
+	 ('fruited'),
+	 ('barrel aged'),
+	 ('New England');
